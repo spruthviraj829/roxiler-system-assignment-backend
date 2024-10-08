912174ca86d9cfb156a07f71bd0ae8db797234d8
pruthviraj <spruthviraj829@gmail.com>
2024-10-09 01:02:37 +0530

    new commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require('cors');
-
-app.listen(process.env.port ,()=>{
+const PORT =process.env.port || 5000 ;
+app.listen( PORT,()=>{
      console.log(`app is running on port ${process.env.port}`);
 })
 
